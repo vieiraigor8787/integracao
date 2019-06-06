78c0c1a58cd132a6d47aeb372b49492dc9165163
vieiraigor8787 <igor_vc@hotmail.com>
2019-06-06 12:44:01 -0300

    components, rotas e arquivos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UsuarioComponent } from './usuario/usuario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', 
+    component: HomeComponent
+  },
+  { 
+    path: 'usuario', 
+    component: UsuarioComponent
+  },
+  { 
+    path: '', 
+    pathMatch: 'full', 
+    redirectTo: '/home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { UsuarioComponent } from './usuario/usuario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    FooterComponent,
+    HomeComponent,
+    UsuarioComponent
   ],
   imports: [
     BrowserModule,
